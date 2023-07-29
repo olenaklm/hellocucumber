65eff9a39f02a275057e412ce7782d8074b6a8b9
Olena_Klymenko <lenaklymenko9@gmail.com>
2023-07-29 15:26:23 +0300

    Lesson 25 BDD Cucumber 10 min

---
+++ src/test/java/hellocucumber/RunCucumberTest.java
@@ -0,0 +1,22 @@
+package hellocucumber;
+
+import io.cucumber.junit.CucumberOptions;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("hellocucumber")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@CucumberOptions(
+        plugin = {
+                "pretty", "json:target/Cucumber.json",
+                "html:target/cucumber-html-report"
+        }
+)
+public class RunCucumberTest {
+}
---
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -0,0 +1,58 @@
+package hellocucumber;
+
+import io.cucumber.java.en.*;
+
+
+import static org.junit.Assert.assertEquals;
+
+
+public class StepDefinitions {
+
+    @Given("an example scenario")
+    public void anExampleScenario() {
+    }
+
+    @When("all step definitions are implemented")
+    public void allStepDefinitionsAreImplemented() {
+    }
+
+    @Then("the scenario passes")
+    public void theScenarioPasses() {
+    }
+
+    private String today;
+    private String actualAnswer;
+
+//    @Given("today is Sunday")
+//    public void today_is_Sunday() {
+//        today = "Sunday";
+//    }
+
+    @Given("today is {string}")
+    public void today_is(String today) {
+        this.today = today;
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+
+//    @Given("today is Friday")
+//    public void today_is_Friday() {
+//        today = "Friday";
+//    }
+
+}
+
+class IsItFriday {
+    static String isItFriday(String today) {
+        return "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+}
+
